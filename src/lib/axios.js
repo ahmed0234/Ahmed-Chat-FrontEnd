5e7ea3ecf8f8e1c78b7b64b957e6caa9a538c180
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-11-21 23:56:26 +0500

    errgone

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://ahmedchatapp.vercel.app",
+  baseURL: "https://ahmed-chat-backend-production.up.railway.app",
   withCredentials: true,
 });
