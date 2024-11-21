2d5a3d1eeeaaa399bf682bc2e5319a0306dab7a1
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-11-21 23:59:43 +0500

    errisgonenow

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://ahmed-chat-backend-production.up.railway.app",
+  baseURL: "https://ahmed-chat-backend-production.up.railway.app/api",
   withCredentials: true,
 });
